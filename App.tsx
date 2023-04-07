725a752499ed57728fb8fa87515f6c3e1cb017cc
Jeon-man <gusals236@gmail.com>
2023-04-07 23:15:46 +0900

    FEAT: Add TodoHeader components

--- App.tsx
+++ App.tsx
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
+  const [working, setWorking] = useState<boolean>(true);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "black",
+        paddingHorizontal: 20,
+      }}
+    >
       <StatusBar style="auto" />
+      <TodoHeader working={working} setWorking={setWorking} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+interface TodoHeaderProp {
+  working: boolean;
+  setWorking: (work: boolean) => void;
+}
+
+function TodoHeader({ working, setWorking }: TodoHeaderProp) {
+  function changeWork() {
+    setWorking(true);
+  }
+
+  function changeTravel() {
+    setWorking(false);
+  }
+
+  return (
+    <View style={{ justifyContent: "space-between", flexDirection: "row", marginTop: 100 }}>
+      <TouchableOpacity>
+        <Text
+          style={{ fontSize: 40, fontWeight: "600", color: working ? "white" : "#3A3D40" }}
+          onPress={changeWork}
+        >
+          Work
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text
+          style={{ fontSize: 40, fontWeight: "600", color: !working ? "white" : "#3A3D40" }}
+          onPress={changeTravel}
+        >
+          Travel
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
