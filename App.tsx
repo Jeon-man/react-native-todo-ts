8cd87e7a73d599959421aaa177a0f0a3552f024c
Jeon-man <gusals236@gmail.com>
2023-04-07 23:40:30 +0900

    FEAT: Add TextInput

--- App.tsx
+++ App.tsx
@@ -1,9 +1,41 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function App() {
   const [working, setWorking] = useState<boolean>(true);
+  const [text, setText] = useState<string>("");
+  const [todo, setTodo] = useState<{}>({});
+
+  function changeText(value: string) {
+    setText(value);
+  }
+
+  async function addTodo() {
+    if (text === "") {
+      return;
+    }
+    const newTodo = { ...todo, [Date.now()]: { text, working } };
+    setTodo(newTodo);
+    await saveTodo(newTodo);
+    setText("");
+  }
+
+  async function saveTodo(toSave: {}) {
+    await AsyncStorage.setItem("@todo", JSON.stringify(toSave));
+  }
+
+  async function loadTodo() {
+    const s = await AsyncStorage.getItem("@todo");
+    s !== null ? setTodo(JSON.parse(s)) : null;
+  }
+
+  console.log(todo);
+
+  useEffect(() => {
+    loadTodo();
+  });
 
   return (
     <View
@@ -15,6 +47,21 @@ export default function App() {
     >
       <StatusBar style="auto" />
       <TodoHeader working={working} setWorking={setWorking} />
+      <TextInput
+        style={{
+          backgroundColor: "white",
+          paddingVertical: 10,
+          paddingHorizontal: 20,
+          borderRadius: 30,
+          marginVertical: 20,
+          fontSize: 18,
+        }}
+        placeholder={working ? "Add todo" : "Add travel"}
+        returnKeyType="done"
+        value={text}
+        onChangeText={changeText}
+        onSubmitEditing={addTodo}
+      />
     </View>
   );
 }
